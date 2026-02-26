1a6b761981373c6138dc04f6c40ef770797f7f50
gburton20 <64306503+gburton20@users.noreply.github.com>
2026-02-26 18:16:05 -0500

    Initialised issue, Add Unit Tests for Phase 1 Models

--- backend/carbon/tests.py
+++ backend/carbon/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+class CarbonIntensityRecordValidationTests(TestCase):
+
+class CarbonIntensityRecordIndexTests(TestCase):
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'core',
     'carbon',
     'fleet',
-
 ]
 
 MIDDLEWARE = [
